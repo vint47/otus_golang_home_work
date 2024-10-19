fc473562c3d5c33d71af49076232cdeef03639b5
Vitaliy Karpets <vkarpets@realt.by>
2024-10-19 16:46:10 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,34 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var res strings.Builder
+	for i := 0; i < len(s); i++ {
+		ch := string(s[i])
+		countCh := 1
+
+		if i+1 < len(s) {
+			chNext := string(s[i+1])
+			if numb, err := strconv.Atoi(chNext); err == nil {
+				if _, err := strconv.Atoi(s); err == nil {
+					return "", ErrInvalidString
+				}
+				countCh = numb
+			}
+		}
+
+		if _, err := strconv.Atoi(ch); err != nil {
+			res.WriteString(strings.Repeat(ch, countCh))
+		} else if i == 0 {
+			return "", ErrInvalidString
+		}
+	}
+
+	return res.String(), nil
 }
