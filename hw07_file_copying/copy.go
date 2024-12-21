bfd3823f4e8b38bd4e28ed4fc5f52f722cd94611
Vitaliy Karpets <vkarpets@realt.by>
2024-12-21 22:54:13 +0300

    HW7

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,56 +1,72 @@
 package main
 
-//var (
-//	ErrUnsupportedFile       = errors.New("unsupported file")
-//	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
-//)
-//
-//func Copy(fromPath, toPath string, offset, limit int64) error {
-//	//fromPath = "testdata/input.txt"
-//	//toPath = "out.txt"
-//
-//	fmt.Println("Copy from:", fromPath, "to:", toPath)
-//	fmt.Println("Copy offset:", offset, "limit:", limit)
-//
-//	fileFrom, err := os.Open(fromPath)
-//
-//	if err != nil {
-//		return err
-//	}
-//
-//	fi, err := fileFrom.Stat()
-//	if err != nil {
-//		return err
-//	}
-//	if limit+offset > fi.Size() {
-//		return ErrOffsetExceedsFileSize
-//	}
-//
-//	defer func() {
-//		_ = fileFrom.Close()
-//	}()
-//
-//	fileTo, err := os.Create(toPath)
-//	if err != nil {
-//		return err
-//	}
-//	defer func() {
-//		_ = fileTo.Close()
-//	}()
-//
-//	_, err = fileFrom.Seek(offset, io.SeekStart)
-//	if err != nil {
-//		return err
-//	}
-//
-//	if offset == 0 && limit == 0 {
-//		_, err = io.Copy(fileTo, fileFrom)
-//	} else if offset == 0 {
-//		_, err = io.CopyN(fileTo, fileFrom, limit)
-//	}
-//	if err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
+)
+
+var (
+	ErrUnsupportedFile           = errors.New("unsupported file")
+	ErrOffsetExceedsFileSize     = errors.New("offset exceeds file size")
+	ErrOffsetOrLimitLessThanZero = errors.New("offset or limit less than zero")
+)
+
+func Copy(fromPath, toPath string, offset, limit int64) error {
+	fmt.Println("Copy from:", fromPath, "to:", toPath)
+	fmt.Println("Copy offset:", offset, "limit:", limit)
+
+	if limit < 0 || offset < 0 {
+		return ErrOffsetOrLimitLessThanZero
+	}
+
+	fileFrom, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	fi, err := fileFrom.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	defer func() {
+		_ = fileFrom.Close()
+	}()
+
+	if limit+offset > fi.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer func() {
+		_ = fileTo.Close()
+	}()
+
+	_, err = fileFrom.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	if limit == 0 || limit > fi.Size()-offset {
+		limit = fi.Size() - offset
+	}
+
+	reader := io.LimitReader(fileFrom, limit)
+	progressBar := pb.Full.Start64(limit)
+	defer progressBar.Finish()
+	reader = progressBar.NewProxyReader(reader)
+
+	_, err = io.Copy(fileTo, reader)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
